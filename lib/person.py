222e0a9de82a64bbe2a8470f091f9d146ecdd67c
brightkingpin <luvandabright781@gmail.com>
2023-05-25 17:57:38 +0300

    Done with assignment

--- lib/person.py
+++ lib/person.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
-APPROVED_JOBS = [
+class Person:
+    APPROVED_JOBS = [
     "Admin",
     "Customer Service",
     "Human Resources",
@@ -12,8 +13,34 @@ APPROVED_JOBS = [
     "General Management",
     "Research & Development",
     "Marketing",
-    "Purchasing"
-]
+    "Purchasing"]
 
-class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self._name = None
+        self._job = None
+        if name:
+            self.name = name
+        if job:
+            self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value in Person.APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
